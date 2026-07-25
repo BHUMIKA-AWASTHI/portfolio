fd032e2196636521c1ef923b154cfd49323c0683
Bhumika Awasthi <bhumikaawasthi1@gmail.com>
2026-07-25 19:00:29 +0530

    feat: popup added after submit btn hit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const form = document.getElementById("contactForm");
+const popup = document.getElementById("successPopup");
+const closePopup = document.getElementById("closePopup");
+const button = form.querySelector("button");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  button.disabled = true;
+  button.textContent = "Sending...";
 
   const formData = new FormData(form);
 
@@ -15,15 +21,35 @@ form.addEventListener("submit", async (e) => {
     });
 
     if (response.ok) {
-      alert("Message sent successfully!");
       form.reset();
+
+      button.textContent = "✓ Sent";
+
+      popup.classList.add("show");
+
+      setTimeout(() => {
+        popup.classList.remove("show");
+      }, 4000);
     } else {
       const error = await response.json();
       console.log(error);
       alert(error.error || "Something went wrong.");
+
+      button.textContent = "Submit";
+      button.disabled = false;
     }
   } catch (err) {
     console.error(err);
-    alert("Network error.");
+    alert("Network Error");
+
+    button.textContent = "Submit";
+    button.disabled = false;
   }
+});
+
+closePopup.addEventListener("click", () => {
+  popup.classList.remove("show");
+
+  button.disabled = false;
+  button.textContent = "Submit";
 });
